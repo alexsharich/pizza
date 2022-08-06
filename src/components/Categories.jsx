6f11748b023afb1699675bb17145d2ef25583f52
alexsharich <alexsharich@gmail.com>
2022-08-06 08:23:29 +0200

    init pizza, add styles, activeType, activeSize sort

---
+++ src/components/Categories.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+function Categories(){
+
+const [activeIndex, setActiveIndex] = React.useState(0)
+const categories = ['Все','Мясные','Вегетарианская','Гриль','Острые','Закрытые']
+
+const onClickCategory=(index)=>{
+  setActiveIndex(index)
+}
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((categorie,index)=><li onClick={()=>onClickCategory(index)} className={activeIndex === index ? 'active':''}>{categorie}</li>
+        )}
+        
+      </ul>
+    </div>
+  )
+}
+export default Categories
+
